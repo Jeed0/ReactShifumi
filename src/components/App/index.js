530401667fb810f8bd5f10e6ab82b12da408cf32
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2021-12-28 16:07:27 -0400

    Bases

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,29 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import React from 'react';
+
+// == Components
+import Header from 'src/components/Header';
+import Play from 'src/components/Play';
+import Game from 'src/components/Game';
+import Modal from 'src/components/Modal';
+import Footer from 'src/components/Footer';
+
+
+
+
+
+
+// == Style
+import './styles.scss';
 
 // == Composant
 const App = () => (
-  <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+  <div className="container">
+    <Header />
+    <Play />
+    <Game />
+    <Modal />
+    <Footer />
   </div>
 );
 
