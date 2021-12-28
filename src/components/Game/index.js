034d5b70f065361d9f6fa04187a0eb65275e799d
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2021-12-28 17:44:45 -0400

    game programming result

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -19,10 +19,38 @@ const Game = ({ score, myChoice, setScore }) => {
         newHousePick();
       }, []);
     
+    const Result = () => {
+        if (myChoice === "rock" && house === "scissors") {
+          setPlayerWin("win");
+          setScore(score + 1);
+        } else if (myChoice === "rock" && house === "paper") {
+          setPlayerWin("lose");
+          setScore(score - 1);
+        } else if (myChoice === "scissors" && house === "paper") {
+          setPlayerWin("win");
+          setScore(score + 1);
+        } else if (myChoice === "scissors" && house === "rock") {
+          setPlayerWin("lose");
+          setScore(score - 1);
+        } else if (myChoice === "paper" && house === "rock") {
+          setPlayerWin("win");
+          setScore(score + 1);
+        } else if (myChoice === "paper" && house === "scissors") {
+          setPlayerWin("lose");
+          setScore(score - 1);
+        } else {
+          setPlayerWin("draw");
+        }
+      };
+
     return (
         <div className="game">
         my choice: {myChoice} <br/>
         house choice: {house} <br/>
+        Result: {playerWin} <br/>
+        {playerWin === "win" && <p>You win!</p>}
+        {playerWin === "lose" && <p>You lose!</p>}
+        {playerWin === "draw" && <p>Draw!</p>}
         </div>
     );
     };
