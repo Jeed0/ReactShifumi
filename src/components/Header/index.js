a082e92dc316de281c91d4934160ac1b9b74e356
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2021-12-28 17:52:34 -0400

    score in header ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const Header = () => {
-    return (
-        <div>
-        <h1>Header</h1>
-        </div>
-    )
-    }
+const Header = ({score}) => {
 
-export default Header
+
+
+    return (
+        <div className="header">
+      <div className="text">
+        <span>Rock</span>
+        <span>Paper</span>
+        <span>Scissors</span>
+      </div>
+      <div className="score-box">
+        <span>Score</span>
+        <div className="score-box__score">{score}</div>
+      </div>
+    </div>
+    );
+    };
+
+export default Header;
