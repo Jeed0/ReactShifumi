7da1e63135f01a8afdff575ae931f6109d3c8e41
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2021-12-28 21:39:08 -0400

    End Of Challenge

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,11 +18,8 @@ return (
     <>
       <footer className="footer">
         <div className="attribution">
-          Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-            Frontend Mentor
-          </a>
-          . Coded by <a href="#">Your Name Here</a>.
+          Challenge by Frontend Mentor
+          . Coded by <a href="#">Jee DO</a>
         </div>
         <button className="rules" onClick={toggle}>
           Rules
